2a9e1c088d8c9eb62e90220a99242778993d6487
HOJUN2404 <rlaghwns1995@naver.com>
2022-03-27 19:54:14 +0900

    SQLite3 Study

---
+++ section12.py
@@ -0,0 +1,45 @@
+# Section 12 
+# SQLite 연동, 테이블 생성, 데이터 삽입
+
+import sqlite3
+import datetime
+
+# 삽입 날짜 생성
+now = datetime.datetime.now()
+print('now : ', now)
+
+nowDateTime = now.strftime('%y-%m-%d %H:%M:%S')
+print("nowDateTime : ", nowDateTime)
+
+
+
+print('sqlite3.version : ', sqlite3.version)
+print('sqlite3.sqite_version : ', sqlite3.sqlite_version)
+
+
+# DB 생성 & Auto Commit(Rollback)
+conn = sqlite3.connect('C:/python_class/pythonStudy/resource/database.db', isolation_level=None)
+
+# Cursor
+c = conn.cursor()
+
+print('Cursor Type : ', type(c))
+
+
+# 테이블 생성(Data Type : TEXT, NUMERIC, INTEGER, REAL, BLOB)
+# c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, email TEXT, \
+# phone text, website TEXT, regdate TEXT)")
+
+# Input Date to Table
+# c.execute("INSERT INTO users VALUES(1, 'kim', 'rlaghwns1995@naver.com', '010-6737-2404', 'hojunday.naver.com', ?)", (nowDateTime,))
+# c.execute("INSERT INTO users(id, username, email, phone, website, regdate) VALUES(?, ?, ?, ?, ?, ?)",(2, 'park', 'park@daum.et', '010-1111-1111', 'par.com', nowDateTime))
+
+# Many 삽입 (튜플, 리스트)
+userList = ( \
+    (3, 'Lee', 'lee@naver.com', '010-2222-2222', 'lee.com', nowDateTime),
+    (4, 'cho', 'cho@naver.com', '010-3333-3333', 'cho.com', nowDateTime),
+    (5, 'hwang', 'hwang@naver.com', '010-4444-4444', 'hwang.com', nowDateTime)
+)
+
+c.executemany("INSERT INTO users(id, username, email, phone, website, regdate \
+    VALUES (?,?,?,?,?,?)", userList)
