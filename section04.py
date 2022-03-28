b44c4de760975d0af2f91bc6ca411fcdec5df27c
HOJUN2404 <rlaghwns1995@naver.com>
2022-03-28 23:47:19 +0900

    Python Loop Test

---
+++ section04.py
@@ -0,0 +1,15 @@
+# Loop
+
+# for i in range(1, 10):
+#     print(i)
+
+testList = [1, 3, 5, 7, 9, 11, 13, 15, 16]
+
+for test in testList:
+    if(test % 2 != 1):
+        print(test)
+    elif test < 5:
+        print(test)
+
+        
+
