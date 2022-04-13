b5325a2bb6dab97915583c877854f7bbf5dffabd
HOJUN2404 <rlaghwns1995@naver.com>
2022-04-13 23:17:08 +0900

    coroutine (decorator활용)

--- chapter06_02.py
+++ chapter06_02.py
@@ -14,6 +14,9 @@
 
 # 코루틴 예제1
 
+from calendar import c
+
+
 def coroutine1():
     print('>>> coroutine started !')
     i = yield
@@ -24,14 +27,96 @@ def coroutine1():
 
 c1 = coroutine1()
 
-print('ex1-1 :', c1, type(c1))
+# print('ex1-1 :', c1, type(c1))
 
-next(c1)
+# next(c1)
 
 # 기본으로 None 전달
 # next(c1)
 
-c1.send(100)
+# c1.send(100)
+
+
+# 잘못된 사용
+
+c2 = coroutine1()
+
+
+# c2.send(100)  예외발생
+
+# 코루틴 예제2
+# GEN_CREATED : 처음 대기상태
+# GEN_RUNNING : 실행상태
+# GEN_SUSPENDED : yield 대기상태
+# GEN_CLOSED : 실행 완료 상태
+
+def coroutine2(x):
+    print('>>> coroutine started : {}'.format(x))
+    y = yield x
+    print('>>> coroutine recieved : {}'.format(y))
+    z = yield x + y
+    print('>>> coroutine recieved : {}'.format(z))
+
+
+c3 = coroutine2(10)
+
+from inspect import getgeneratorstate
+
+print('ex1-2 :', getgeneratorstate(c3))
+
+print(next(c3))
+
+print('ex1-3 :', getgeneratorstate(c3))
+
+print(c3.send(15))
+
+# print(c3.send(20))    예외
+
+print()
+print()
+print()
+
+# 데코레이터 패턴
+
+from functools import wraps
+
+def coroutine(func):
+    '''Decorator run until yield'''
+    @wraps(func)
+    def primer(*args, **kwargs):
+        gen = func(*args, **kwargs)
+        next(gen)
+        return gen
+    return primer
+
+
+
+@coroutine
+def sumer():
+    total = 0
+    term = 0
+    while True:
+        term = yield total
+        total += term
+
+
+su = sumer()
+
+print('ex2-1 :', su.send(100))
+print('ex2-2 :', su.send(40))
+print('ex2-3 :', su.send(60))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
