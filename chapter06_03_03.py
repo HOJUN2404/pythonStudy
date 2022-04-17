d8a4bbe087de23a266a65fa7ce1321067a7b6e6e
HOJUN2404 <rlaghwns1995@naver.com>
2022-04-17 22:17:03 +0900

    Python Future..!

--- chapter06_03_03.py
+++ chapter06_03_03.py
@@ -1,18 +1,15 @@
-# Chapter06-3-1
-# 파이썬 심화
+# Chapter 06_03_01
+
 # Future 동시성
 # 비동기 작업 실행
-# 지연시간(Block) CPU 및 리소스 낭비 방지 -> (File)Network I/O 관련 작업 동시성 활용 권장
+# 지연시간(Block) CPU 및 리소스 낭비 방지 -> Network I/O 관련 작업 동시성 활용 권장
 # 적합한 작업일 경우 순차 진행보다 압도적으로 성능 향상
 
-# 실습 대상 3가지 경우
+# 실습대상 3가지 경우
 
-# 순차 실행
-# concurrent.futures 방법1
-# concurrent.futures 방법2
-
-# Google Python GIL(Global Interpreter Lock)
-# Gil은 한 번에 하나의 스레드만 수행 할 수 있게 인터프리터 자체에서 락을 거는 것.
+# 1. 순차실행
+# 2. concurrent.futers  방법1
+# 3. concurrent.futers  방법2
 
 import os
 import time
@@ -21,22 +18,28 @@ import csv
 from concurrent import futures
 
 
-# concurrent.future 방법1(ThreadPoolExecutor, ProcessPoolExecutor)
+# Google Python GIL (Global Interpreter Lock)
+# GIL은 한 번에 하나의 스레드만 수행할 수 있게 인터프리터 자체에서 락을 거는 것.
+
+
+
+# Concurrent.future 방법1 (ThreadPoolExecutor, ProcessPoolExecutor)
 # map()
-# 서로 다른 스레드 또는 프로세스에서 실행 가능
-# 내부 과정 알 필요 없으며, 고수준으로 인터페이스 제공
+# 서로 다른 쓰레드 또는 프로세스에서 살행간능
+# 내부과정 알 필요 없으며, 고수준으로 인터페이스 제공
+
 
 # 국가정보
-NATION_LS = ('Singapore Germany Israel Norway Italy Canada France Spain Mexico').split()
+NATION_LS = ('Singapore Germany Isreal Norway Italy Canada France Spain Mexico').split()
 # 초기 CSV 위치
-TARGET_CSV = 'C:/python_advanced/resources/nations.csv' # 본인 경로 변경
-# 저장 폴더 위치
-DEST_DIR = 'C:/python_advanced/csvs' # 본인 경로 변경
-# CSV 헤더 기초 정보
+TARGET_CSV = 'C:/hjProgramming/pythonStudy/resource/nations.csv'
+# 저장폴더 위치
+DEST_DIR = 'C:/hjProgramming/pythonStudy/csvs'
+# CSV 헤더 위치
 HEADER = ['Region','Country','Item Type','Sales Channel','Order Priority','Order Date','Order ID','Ship Date','Units Sold','Unit Price','Unit Cost','Total Revenue','Total Cost','Total Profit']
 
 
-# 국가별 CSV 파일 저장
+# 국가별 csv 파일 저장
 def save_csv(data, filename):
     # 최종 경로 생성
     path = os.path.join(DEST_DIR, filename)
@@ -50,12 +53,11 @@ def save_csv(data, filename):
             writer.writerow(row)
 
 
-
 # 국가별 분리
 def get_sales_data(nt):
     with open(TARGET_CSV, 'r') as f:
         reader = csv.DictReader(f)
-        # Dict을 리스트로 적재
+        # Dict를 리스트로 적재
         data = []
         # Header 확인
         # print(reader.fieldnames)
@@ -67,14 +69,15 @@ def get_sales_data(nt):
                 data.append(r)
     return data
 
-# 중간 상황 출력
+
 def show(text):
     print(text, end=' ')
-    # 중간 출력(버퍼 비우기)
+    # 중간출력(버퍼 비우기)
     sys.stdout.flush()
 
 
-# 국가 별 분리 함수 실행
+
+# 국가별 분리 함수 실행
 def separate_many(nt):
     # 분리 데이터
     data = get_sales_data(nt)
@@ -83,27 +86,30 @@ def separate_many(nt):
     # 파일 저장
     save_csv(data, nt.lower() + '.csv')
 
-    return nt
+    return len(nt)
 
 
-# 시간 측정 및 메인함수
+# 시간측정 및 메인함수
 def main(separate_many):
     # worker 개수
     worker = min(20, len(NATION_LS))
-    # 시작 시간
+
+    # 시작시간
     start_tm = time.time()
-    # 결과 건수
-    # ProcessPoolExecutor : GIL 우회, 변경 후 -> os.cpu_count()
-    # ThreadPoolExecutor : GIL 종속
-    with futures.ProcessPoolExecutor() as excutor:
-        # map -> 작업 순서 유지, 즉시 실행
-        result_cnt = excutor.map(separate_many, sorted(NATION_LS))
-    # 종료 시간
+    # 결과건수
+    # ProcessPoolExecutor : GIL 우회, 변경후 - >
+    # ThreadPoolExcutor :  GIL 종속
+    with futures.ThreadPoolExecutor() as executor:
+        # map -> 작업순서를 유지하고 즉시 실행
+        result_cnt = executor.map(separate_many, sorted(NATION_LS))
+    # 종료시간
     end_tm = time.time() - start_tm
 
     msg = '\n{} csv separated in {:.2f}s'
-    # 최종 결과 출력
+
     print(msg.format(list(result_cnt), end_tm))
+
+
 
 
 # 실행
